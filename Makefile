4a59e9371b4ace10cd24bb548ca8e39ac947f7fd
philpill <github@philpill.net>
2020-10-02 23:26:08 +0100

    stb_image

--- Makefile
+++ Makefile
@@ -1,27 +1,29 @@
 CFLAGS=-std=gnu99
 SHELL = /bin/sh
 CC = gcc
-
+DEPS = src/stb_image.h
 OBJS = src/main.c
 
-INCLUDE_PATHS = -IC:\dev\SDL2-2.0.12\i686-w64-mingw32\include\SDL2 -IC:\dev\SDL2_image-2.0.5\i686-w64-mingw32\include\SDL2
+INCLUDE_PATHS = -IC:\dev\SDL2-2.0.12\i686-w64-mingw32\include\SDL2
 
-LIBRARY_PATHS = -LC:\dev\SDL2-2.0.12\i686-w64-mingw32\lib -LC:\dev\SDL2_image-2.0.5\i686-w64-mingw32\lib
+LIBRARY_PATHS = -LC:\dev\SDL2-2.0.12\i686-w64-mingw32\lib
 
 # https://stackoverflow.com/a/34540492
 # COMPILER_FLAGS = -w -Wl,-subsystem,windows
 COMPILER_FLAGS = -w
 
-LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2
 
 OBJ_NAME = tbgc
+
+.PHONY: all clean
 
 all: build run
 
 run:
-	$(OBJ_NAME).exe
+	$(OBJ_NAME)
 
-build:$(OBJS)
+build:$(OBJS) $(DEPS)
 	$(CC) $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
 
 clean:
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <SDL.h>
-#include <SDL_image.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -11,42 +12,7 @@ const int SCREEN_TICK_PER_FRAME = 1000 / 30;
 SDL_Window *window = NULL;
 SDL_Surface *screenSurface = NULL;
 SDL_Surface *pngSurface = NULL;
-
-bool init()
-{
-    bool success = true;
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("init error: SDL_Error: %s\n", SDL_GetError());
-        success = false;
-    }
-    else
-    {
-        window = SDL_CreateWindow("TBGC", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("create window error: SDL_Error: %s\n", SDL_GetError());
-            success = false;
-        }
-        else
-        {
-            //Initialize PNG loading
-            int imgFlags = IMG_INIT_PNG;
-            if (!(IMG_Init(imgFlags) & imgFlags))
-            {
-                printf("SDL_image Error: %s\n", IMG_GetError());
-                success = false;
-            }
-            else
-            {
-                screenSurface = SDL_GetWindowSurface(window);
-            }
-        }
-    }
-
-    return success;
-}
+unsigned char *stbiData = NULL;
 
 void close()
 {
@@ -56,97 +22,112 @@ void close()
     SDL_DestroyWindow(window);
     window = NULL;
 
-    IMG_Quit();
+    stbi_image_free(stbiData);
+    stbiData = NULL;
+
     SDL_Quit();
-}
-
-SDL_Surface *loadSurface(char *path)
-{
-    SDL_Surface *optimisedSurface = NULL;
-
-    SDL_Surface *loadedSurface = IMG_Load(path);
-    if (loadedSurface == NULL)
-    {
-        printf("Unable to load image. SDL_image Error: %s\n", IMG_GetError());
-    }
-    else
-    {
-        optimisedSurface = SDL_ConvertSurface(loadedSurface, screenSurface->format, 0);
-        if (optimisedSurface == NULL)
-        {
-            printf("Unable to optimise image. SDL_image Error: %s\n", IMG_GetError());
-        }
-
-        SDL_FreeSurface(loadedSurface);
-    }
-
-    return optimisedSurface;
-}
-
-bool loadMedia()
-{
-    bool success = true;
-
-    //Load PNG surface
-    pngSurface = loadSurface("assets/1.png");
-    if (pngSurface == NULL)
-    {
-        printf("Failed to load PNG image!\n");
-        success = false;
-    }
-
-    return success;
 }
 
 int main(int argc, char *args[])
 {
-    if (!init())
-    {
-        printf("Failed to initialize!\n");
+    bool quit = false;
+    SDL_Event e;
+
+    double start_t, end_t, total_t;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("TBGC", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    screenSurface = SDL_GetWindowSurface(window);
+
+    int width, height, orig_format;
+    int req_format = STBI_rgb_alpha;
+    stbiData = stbi_load("assets/1.png", &width, &height, &orig_format, req_format);
+
+    Uint32 rmask, gmask, bmask, amask;
+    #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+        int shift = (req_format == STBI_rgb) ? 8 : 0;
+        rmask = 0xff000000 >> shift;
+        gmask = 0x00ff0000 >> shift;
+        bmask = 0x0000ff00 >> shift;
+        amask = 0x000000ff >> shift;
+    #else // little endian, like x86
+        rmask = 0x000000ff;
+        gmask = 0x0000ff00;
+        bmask = 0x00ff0000;
+        amask = (req_format == STBI_rgb) ? 0 : 0xff000000;
+    #endif
+
+    int depth, pitch;
+    if (req_format == STBI_rgb) {
+        depth = 24;
+        pitch = 3*width; // 3 bytes per pixel * pixels per row
+    } else { // STBI_rgb_alpha (RGBA)
+        depth = 32;
+        pitch = 4*width;
     }
-    else
+
+    pngSurface = SDL_CreateRGBSurfaceFrom((void*)stbiData, width, height, depth, pitch,
+                                            rmask, gmask, bmask, amask);
+
+
+    // start timer
+    start_t = SDL_GetTicks();
+
+    while (!quit)
     {
-        if (!loadMedia())
+        while (SDL_PollEvent(&e) != 0)
         {
-            printf("Failed to load media!\n");
-        }
-        else
-        {
-            bool quit = false;
-            SDL_Event e;
-
-            Uint32 start_t, end_t, total_t;
-
-            // start timer
-            start_t = SDL_GetTicks();
-
-            while (!quit)
+            if (e.type == SDL_QUIT)
             {
-                while (SDL_PollEvent(&e) != 0)
+                quit = true;
+            }
+            else if (e.type == SDL_KEYDOWN)
+            {
+                //Select surfaces based on key press
+                switch (e.key.keysym.sym)
                 {
-                    if (e.type == SDL_QUIT)
-                    {
-                        quit = true;
-                    }
-                }
+                case SDLK_UP:
+                    printf("u\n");
+                    break;
 
-                SDL_BlitSurface(pngSurface, NULL, screenSurface, NULL);
+                case SDLK_DOWN:
+                    printf("d\n");
+                    break;
 
-                SDL_UpdateWindowSurface(window);
+                case SDLK_LEFT:
+                    printf("l\n");
+                    break;
 
-                end_t = SDL_GetTicks();
+                case SDLK_RIGHT:
+                    printf("r\n");
+                    break;
 
-                total_t = end_t - start_t;
-
-                if (total_t < SCREEN_TICK_PER_FRAME)
-                {
-                    printf("%d\n", SDL_GetTicks());
-
-                    SDL_Delay(SCREEN_TICK_PER_FRAME - total_t);
-
-                    start_t = SDL_GetTicks();
+                case SDLK_ESCAPE:
+                    close();
+                    return 0;
                 }
             }
+        }
+
+        SDL_BlitSurface(pngSurface, NULL, screenSurface, NULL);
+
+        SDL_UpdateWindowSurface(window);
+
+        end_t = SDL_GetTicks();
+
+        total_t = end_t - start_t;
+
+        // printf("%d - %d = %d\n", end_t, start_t, total_t);
+
+        if (total_t < SCREEN_TICK_PER_FRAME)
+        {
+            // printf("delay: %d\n", SCREEN_TICK_PER_FRAME - total_t);
+
+            SDL_Delay(SCREEN_TICK_PER_FRAME - total_t);
+
+            start_t = SDL_GetTicks();
         }
     }
 
