d14df44c26bab4748412eb6d90521d9ae37a754c
philpill <github@philpill.net>
2020-10-03 19:29:53 +0100

    user input

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <SDL.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 #include <stdio.h>
 #include <stdbool.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -10,20 +11,22 @@ const int SCREEN_HEIGHT = 480;
 const int SCREEN_TICK_PER_FRAME = 1000 / 30;
 
 SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 SDL_Surface *screenSurface = NULL;
 SDL_Surface *pngSurface = NULL;
+SDL_Texture *pngTexture = NULL;
 unsigned char *stbiData = NULL;
 
 void close()
 {
-    SDL_FreeSurface(pngSurface);
-    pngSurface = NULL;
+    SDL_DestroyTexture(pngTexture);
+    pngTexture = NULL;
+
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
 
     SDL_DestroyWindow(window);
     window = NULL;
-
-    stbi_image_free(stbiData);
-    stbiData = NULL;
 
     SDL_Quit();
 }
@@ -39,7 +42,12 @@ int main(int argc, char *args[])
 
     window = SDL_CreateWindow("TBGC", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
     screenSurface = SDL_GetWindowSurface(window);
+
+    int posx = 0;
+    int posy = 0;
 
     int width, height, orig_format;
     int req_format = STBI_rgb_alpha;
@@ -68,9 +76,19 @@ int main(int argc, char *args[])
         pitch = 4*width;
     }
 
+    // printf("width: %d\n", width);
+    // printf("height: %d\n", height);
+
     pngSurface = SDL_CreateRGBSurfaceFrom((void*)stbiData, width, height, depth, pitch,
                                             rmask, gmask, bmask, amask);
 
+    stbi_image_free(stbiData);
+    stbiData = NULL;
+
+    pngTexture = SDL_CreateTextureFromSurface(renderer, pngSurface);
+
+    SDL_FreeSurface(pngSurface);
+    pngSurface = NULL;
 
     // start timer
     start_t = SDL_GetTicks();
@@ -89,19 +107,19 @@ int main(int argc, char *args[])
                 switch (e.key.keysym.sym)
                 {
                 case SDLK_UP:
-                    printf("u\n");
+                    posy--;
                     break;
 
                 case SDLK_DOWN:
-                    printf("d\n");
+                    posy++;
                     break;
 
                 case SDLK_LEFT:
-                    printf("l\n");
+                    posx--;
                     break;
 
                 case SDLK_RIGHT:
-                    printf("r\n");
+                    posx++;
                     break;
 
                 case SDLK_ESCAPE:
@@ -111,9 +129,15 @@ int main(int argc, char *args[])
             }
         }
 
-        SDL_BlitSurface(pngSurface, NULL, screenSurface, NULL);
+        SDL_Rect dst;
+        dst.x = posx;
+        dst.y = posy;
+        dst.w = 32;
+        dst.h = 32;
 
-        SDL_UpdateWindowSurface(window);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, pngTexture, NULL, &dst);
+        SDL_RenderPresent(renderer);
 
         end_t = SDL_GetTicks();
 
